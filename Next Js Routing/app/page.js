31c488712341a98a8f689bb7a322b567316f5e6c
Sonu Kumar <sksonu8544@gmail.com>
2024-02-25 19:52:20 +0530

    Next js Routing

---
+++ Next Js Routing/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-[30px] text-center mt-[100px]" >Hello Sonu...</h1>
+    </>
+
+  );
+}
